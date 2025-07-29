57bca3f0cc9ea489cd16d330b9c61b72c56358ac
beechang <dbsckd92@naver.com>
2025-07-29 14:54:00 +0900

    feat : 웹알티씨 인텐트 액션용 enum 추가

---
+++ app/src/main/java/example/beechang/together/webrtc/intent/TogeWebRtcIntent.kt
@@ -0,0 +1,6 @@
+package example.beechang.together.webrtc.intent
+
+enum class TogeWebRtcIntent(val value: String) {
+    START("example.beechang.together.webrtc.START"),
+    STOP("example.beechang.together.webrtc.STOP")
+}
