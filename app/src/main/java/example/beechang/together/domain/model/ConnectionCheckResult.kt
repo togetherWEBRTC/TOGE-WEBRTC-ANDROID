e1b22900974e4c0a674c08d875814effce1ac292
beechang <dbsckd92@naver.com>
2025-09-29 23:57:10 +0900

    remove : 미사용 제거

--- app/src/main/java/example/beechang/together/domain/model/ConnectionCheckResult.kt
+++
@@ -1,7 +0,0 @@
-package example.beechang.together.domain.model
-
-data class ConnectionCheckResult(
-    val isDuplicateConnection: Boolean = false,
-    val existingSocketId: String? = null,
-    val currentSocketId: String? = null,
-)
